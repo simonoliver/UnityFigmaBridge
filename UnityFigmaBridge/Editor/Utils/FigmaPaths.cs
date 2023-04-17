82067ec312bbe113d80d78422c03a1f1feeeaf82
Simon Oliver <simon@handcircus.com>
2023-04-17 16:14:40 +0100

    Fixed up issue with invalid component paths, and allowed for paths with slash character within

--- UnityFigmaBridge/Editor/Utils/FigmaPaths.cs
+++ UnityFigmaBridge/Editor/Utils/FigmaPaths.cs
@@ -91,21 +91,17 @@ namespace UnityFigmaBridge.Editor.Utils
 
         private static string ReplaceUnsafeCharacters(string inputFilename)
         {
-            var safeFilename=inputFilename;
-            // Remove path extensions
-            var split = safeFilename.Split('/');
-            safeFilename = split.Last();
+            // We want to trim spaces from start and end of filename, or we'll throw an error
+            // We no longer want to use the final "/" character as this might be used by the user
+            var safeFilename=inputFilename.Trim();
             return MakeValidFileName(safeFilename);
-            //safeFilename = safeFilename.Replace(".", "_");
-            //safeFilename = safeFilename.Replace(" ", "_");
-            //safeFilename = safeFilename.Replace(":", "_");
-            //return safeFilename;
         }
         
         // From https://www.csharp-console-examples.com/general/c-replace-invalid-filename-characters/
         public static string MakeValidFileName(string name)
         {
-            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(Path.GetInvalidFileNameChars()));
+            invalidChars += ".";
             string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
  
             return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
